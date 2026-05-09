d483a302544a974bb3f4b2eac221ffbf7f18a030
Himanshukabsuri <hkabsuri@gmail.com>
2026-05-09 20:21:48 +0530

    add middleware

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 
-
+import jwt from 'jsonwebtoken'
 
 
 // verify token
@@ -18,8 +18,36 @@ export const protect = async(req,res,next)=>{
 
         // remove bearer
 
-        
+        const realToken = token.split(" ")[1];
+
+        const decoded = jwt.verify(realToken,process.env.JWT_SECRET);
+
+        req.user = decoded;
+
+        next()
     } catch (error) {
-        
+        res.status(401).json({
+      success: false,
+      message: "Invalid token",
+    });
+    }
+}
+
+// admin only 
+
+export const adminOnly = (req,res,next)=>{
+    try {
+        if (req.user.role !== "admin") {
+             return res.status(403).json({
+        success: false,
+        message: "Admin access only",
+      });
+        }
+        next()
+    } catch (error) {
+        res.status(500).json({
+      success: false,
+      message: error.message,
+    });
     }
 }
